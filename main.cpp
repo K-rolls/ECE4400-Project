e69136344a4e3f87e7fb4f02bd7f6b4f7fde9ac5
Keegan <keeganrolls@gmail.com>
2022-07-30 22:50:47 -0230

    Able to read from inventory.json now

--- main.cpp
+++ main.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
-#include "jsoncpp/dist/json/json.h"
-#include <tchar.h>
+#include "jsoncpp/include/json/json.h"
+#include "jsoncpp/include/json/value.h"
 #include <fstream>
 
 using namespace std;
 
-int main(int argc, _TCHAR *argv[]) {
-    Json::Reader reader;
-    Json::Value root;
-    ifstream inputJson("testjson.json", std::ifstream::binary);
-    bool parseSuccess = reader.parse(inputJson, root, false);
-    if (parseSuccess) {
-        const Json::Value resultValue = root["result"];
-        cout << "Result is " << resultValue << "\n";
-        cout << root;
+int main() {
+    Json::Reader reader;             // for reading the data
+    Json::Value newValue;            // for modifying and storing new values
+    Json::StyledStreamWriter writer; // for writing in json files
+    Json::Value message;
+    ofstream newFile;
+
+    // Updating the json data
+    newValue["Category"] = "Technical";
+
+    // we can add new values as well
+    newValue["first"] = "Shishank";
+    newValue["last"] = "Jain";
+
+    message["info"] = newValue;
+    // make the changes in a new file/original file
+    //    newFile.open("items.json");
+    //    writer.write(newFile, newValue);
+    //    writer.write(newFile, message);
+    //    newFile.close();
+
+    // opening file using fstream
+    ifstream file("inventory.json");
+
+    // check if there is any error is getting data from the json file
+    if (!reader.parse(file, newValue)) {
+        cout << reader.getFormattedErrorMessages();
+        exit(1);
+    } else {
+
+        cout << newValue << endl;
     }
-    Json::StyledWriter styledWriter;
-    Json::FastWriter fastWriter;
-    Json::Value newValue;
-    newValue["result"] = "ok";
-    cout << styledWriter.write(newValue) << "\n";
-    cout << fastWriter.write(newValue) << "\n";
-    Json::Value value;
-    value["test"] = "email";
-    cout << value;
+    for (auto msg: newValue) {
+        for (auto dat: msg) {
+            //Insert time, name, messsage,priority, urgency,key
+            if (msg["Name"] == dat) {
+                cout << "Name: " << dat << endl;
+            } else if (msg["Category"] == dat) {
+                cout << "Category: " << dat << endl;
+            } else if (msg["Price"] == dat) {
+                cout << "Price: " << dat << endl;
+            } else if (msg["Expiry Date"] == dat) {
+                cout << "Expiry Date: " << dat << endl;
+            } else if (msg["Count"] == dat) {
+                cout << "Count: " << dat << endl;
+            }
+        }
+        cout << "\n";
+    }
     return 0;
 }
